c9450d19e8443d2ef8713ecde3391768a506bc0e
yasarcandamli <yasarcandamli@gmail.com>
2024-09-27 12:46:17 +0300

    Conditional Tests - Coding - @Disabled and @EnabledOnOs

---
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -0,0 +1,39 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+class ConditionalTest {
+
+    @Test
+    @Disabled("Don't run until JIRA #123 is resolved")
+    void basicTest() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testForWindowsOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void testForMacOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS, OS.MAC})
+    void testForWindowsAndMacOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testForLinuxOnly() {
+        // execute method and perform asserts
+    }
+}
