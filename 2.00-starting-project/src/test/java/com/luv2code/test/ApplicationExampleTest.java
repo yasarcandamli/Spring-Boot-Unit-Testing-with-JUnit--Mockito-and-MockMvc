f07944269e1981ced197b28bceb0a0621b7537d3
yasarcandamli <yasarcandamli@gmail.com>
2024-09-27 14:54:31 +0300

    Spring Boot Unit Testing - Read application.properties and Inject Spring Beans

--- 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -1,11 +1,51 @@
 package com.luv2code.test;
 
 import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
 public class ApplicationExampleTest {
+
+    private static int count = 0;
+
+    @Value("${info.app.name}")
+    private String appInfo;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.school.name}")
+    private String SchoolName;
+
+    @Autowired
+    CollegeStudent student;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+    @BeforeEach
+    public void beforeEach() {
+        count += 1;
+        System.out.println("Testing: " + appInfo + " which is " + appDescription +
+                " Version: " + appVersion + ". Execution of test method " + count);
+        student.setFirstname("Eric");
+        student.setLastname("Roby");
+        student.setEmailAddress("eric.roby@luv2code_school.com");
+        studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.00, 85.00, 76.50, 91.75)));
+        student.setStudentGrades(studentGrades);
+    }
 
     @Test
     void basicTest() {
