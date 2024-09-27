6c941baeab4e5e5ad3f6b14873005261f91c8952
yasarcandamli <yasarcandamli@gmail.com>
2024-09-27 13:43:22 +0300

    FizzBuzz Project- Coding - Refactoring

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -7,6 +7,25 @@ public class FizzBuzz {
     // If number is divisible by 3 and 5, print FizzBuzz
     // If number is NOT divisible by 3 or 5, then print the number
 
+
+    public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.isEmpty()) {
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+/*
     public static String compute(int i) {
 
         if ((i % 3 == 0) && (i % 5 == 0)) {
@@ -19,4 +38,5 @@ public class FizzBuzz {
             return Integer.toString(i);
         }
     }
+    */
 }
