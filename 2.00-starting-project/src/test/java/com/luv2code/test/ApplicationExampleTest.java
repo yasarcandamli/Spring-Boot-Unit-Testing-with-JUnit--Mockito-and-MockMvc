f5c92b0ab4d54565924410027ca587cb44802660
yasarcandamli <yasarcandamli@gmail.com>
2024-09-27 14:43:42 +0300

    Spring Boot Unit Testing - Project Set Up & Adding Maven Dependency

---
+++ 2.00-starting-project/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -0,0 +1,14 @@
+package com.luv2code.test;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class ApplicationExampleTest {
+
+    @Test
+    void basicTest() {
+
+    }
+}
