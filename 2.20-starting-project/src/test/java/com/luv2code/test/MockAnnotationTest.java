81aca5cecf4f04ab4d82986d1113b6b96ac9b355
yasarcandamli <yasarcandamli@gmail.com>
2024-10-07 14:20:15 +0300

    MockBean

--- 2.20-starting-project/src/test/java/com/luv2code/test/MockAnnotationTest.java
+++ 2.20-starting-project/src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -12,9 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
@@ -29,10 +31,12 @@ public class MockAnnotationTest {
     @Autowired
     StudentGrades studentGrades;
 
-    @Mock
+    // @Mock
+    @MockBean
     private ApplicationDao applicationDao;
 
-    @InjectMocks
+    // @InjectMocks
+    @Autowired
     private ApplicationService applicationService;
 
     @BeforeEach
@@ -57,5 +61,23 @@ public class MockAnnotationTest {
 
         verify(applicationDao, times(1)).addGradeResultsForSingleClass(
                                                                         studentGrades.getMathGradeResults());
+    }
+
+    @DisplayName("Find Gpa")
+    @Test
+    public void assertEqualsTestFindGpa() {
+        when(applicationDao.findGradePointAverage(studentGrades.getMathGradeResults()))
+                .thenReturn(88.31);
+        assertEquals(88.31, applicationService.findGradePointAverage(studentOne
+                .getStudentGrades().getMathGradeResults()));
+    }
+
+    @DisplayName("Not Null")
+    @Test
+    public void testAssertNotNull() {
+        when(applicationDao.checkNull(studentGrades.getMathGradeResults()))
+                .thenReturn(true);
+        assertNotNull(applicationService.checkNull(studentOne.getStudentGrades()
+                .getMathGradeResults()), "Object should not be null");
     }
 }
