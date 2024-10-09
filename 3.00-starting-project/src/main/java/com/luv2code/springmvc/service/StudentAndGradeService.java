5b17a2a351e6a1eff2a694e8a4c349d33691f5c2
yasarcandamli <yasarcandamli@gmail.com>
2024-10-09 11:39:50 +0300

    Testing Spring Boot MVC Web Apps - Refactor Code for Configure Information Model

--- 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
+++ 3.00-starting-project/src/main/java/com/luv2code/springmvc/service/StudentAndGradeService.java
@@ -8,6 +8,7 @@ import com.luv2code.springmvc.repository.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
@@ -163,5 +164,35 @@ public class StudentAndGradeService {
                 studentGrades);
 
         return gradebookCollegeStudent;
+    }
+
+    public void configureStudentInformationModel(int id, Model m) {
+        GradebookCollegeStudent studentEntity = studentInformation(id);
+
+        m.addAttribute("student", studentEntity);
+
+        if (studentEntity.getStudentGrades().getMathGradeResults().size() > 0) {
+            m.addAttribute("mathAverage", studentEntity.getStudentGrades().findGradePointAverage(
+                    studentEntity.getStudentGrades().getMathGradeResults()
+            ));
+        } else {
+            m.addAttribute("mathAverage", "N/A");
+        }
+
+        if (studentEntity.getStudentGrades().getScienceGradeResults().size() > 0) {
+            m.addAttribute("scienceAverage", studentEntity.getStudentGrades().findGradePointAverage(
+                    studentEntity.getStudentGrades().getScienceGradeResults()
+            ));
+        } else {
+            m.addAttribute("scienceAverage", "N/A");
+        }
+
+        if (studentEntity.getStudentGrades().getHistoryGradeResults().size() > 0) {
+            m.addAttribute("historyAverage", studentEntity.getStudentGrades().findGradePointAverage(
+                    studentEntity.getStudentGrades().getHistoryGradeResults()
+            ));
+        } else {
+            m.addAttribute("historyAverage", "N/A");
+        }
     }
 }
